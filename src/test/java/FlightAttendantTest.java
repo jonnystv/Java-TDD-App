68e46eef4c9a67c1cc43c7d6592377e5d02dfbd1
jonnystv <jonathan@darefoundation.com>
2021-02-15 02:29:54 +0000

    FlightAttendant hasName passed

---
+++ src/test/java/FlightAttendantTest.java
@@ -0,0 +1,24 @@
+import Aircrew.Aircrew;
+import Aircrew.FlightAttendant;
+import Aircrew.Rank;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlightAttendantTest {
+
+    FlightAttendant flightAttendant;
+
+    @Before
+    public void before() {
+        flightAttendant = new FlightAttendant("Bob Smith",Rank.CHIEF_ATTENDANT);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Bob Smith", flightAttendant.getName());
+    }
+}
+
+
