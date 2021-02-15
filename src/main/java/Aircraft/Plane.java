8eaf79f3ea0a99d079188daa409aa72727d64d4b
jonnystv <jonathan@darefoundation.com>
2021-02-15 00:33:46 +0000

    Aircraft package created

--- src/main/java/Aircraft/Plane.java
+++ src/main/java/Aircraft/Plane.java
@@ -1,4 +1,16 @@
 package Aircraft;
 
 public class Plane {
+
+    private PlaneType planeType;
+    private PlaneType passengerCapacity;
+    private PlaneType weightLimit;
+    private String name;
+
+    public Plane(String name, PlaneType planeType, PlaneType passengerCapacity, PlaneType weightLimit) {
+        this.planeType = planeType;
+        this.passengerCapacity = passengerCapacity;
+        this.weightLimit = weightLimit;
+        this.name = name;
+    }
 }
--- src/main/java/Aircraft/PlaneType.java
+++ src/main/java/Aircraft/PlaneType.java
@@ -1,4 +1,16 @@
 package Aircraft;
 
 public enum PlaneType {
+
+    B747(30, 300.00),
+    B767(10, 100.00),
+    B787(20, 200.00);
+
+    private final int passengerCapacity;
+    private final double weightLimit;
+
+    PlaneType(int passengerCapacity, double weightLimit) {
+        this.passengerCapacity = passengerCapacity;
+        this.weightLimit = weightLimit;
+    }
 }
